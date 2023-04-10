51c64c97f26c57ae8c0860c001650d9d48f8acf9
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-10 15:04:36 +0530

    initial commit

--- backend/db_connnection.js
+++ backend/db_connnection.js
@@ -4,13 +4,14 @@ const mongoUri = 'mongodb+srv://fevfood:fevfood@cluster0.fanyayv.mongodb.net/fev
 const Connectdb = async ()=>{
     try{
         const connect = await mongoose.connect(mongoUri);
-        console.log("Database name",connect.connection.host,connect.connection.name);
+        // console.log("Database name",connect.connection.host,connect.connection.name);
 
         const fetched_data = mongoose.connection.db.collection("db_items");
 
         const items = await fetched_data.find({}).toArray(); // Await the result of the find() method
 
         console.log(items);
+        // console.log('connnected');
     }
     catch(err){
         console.log(err);
